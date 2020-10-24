3bf06ea7be1f298f64ef781527f75ef6b85de53a
이민호/TL개발팀 <thinkub0219@gmail.com>
2020-10-24 11:43:05 +0900

    queryDsl 설정

---
+++ src/main/java/com/ming/shopping/product/config/QueryDslConfig.java
@@ -0,0 +1,23 @@
+package com.ming.shopping.product.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author by Ming(thinkub0219@gmail.com) on 2020. 10. 24..
+ */
+
+@Configuration
+public class QueryDslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
